09459f24cf210db0275e2ad129e8e23ba91ec17c
indogegewepoe <bagasuwaidha007@gmail.com>
2025-05-13 13:22:30 +0700

    hero progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeaderSearch, HeaderMegaMenu } from '@/components/HeaderSearch/HeaderSearch';
+import { HeroImageRight } from '@/components/HeroImageRight/HeroImageRight'; 
 
 export default function HomePage() {
   return (
     <>
       <HeaderSearch />
       <HeaderMegaMenu />
+      <HeroImageRight />
     </>
   );
 }
